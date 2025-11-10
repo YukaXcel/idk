baa61f982220ad2eaed8d5ce62341533e474be14
ゆか <bchayadaheangha@gmail.com>
2025-11-10 16:19:35 +0700

    just some sillies changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkbieaw() {
     const text = inputText.value ;
-    if (!text) return result.innerHTML = "pls type smth" ;
+    if (!text) return result.innerHTML = "พิมพ์ก่อนน้าาา" ;
     const n = (Math.random() * 100).toFixed(2);
     result.innerHTML = `ความเบียวของคุณคือ: ${n}%` ;
 }
